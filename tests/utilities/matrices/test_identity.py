b62489a25e3f0fa171b8b56208148a453f799e9b
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2025-01-09 18:03:18 +0000

    Add test for matrix multiplication with identity

--- tests/utilities/matrices/test_identity.py
+++ tests/utilities/matrices/test_identity.py
@@ -1,6 +1,12 @@
 import qsharp # type: ignore
+import pytest
+import numpy as np
+
+from .test_multiply import matrix_dot_product_code
 qsharp.init(project_root = "./QSharpUtilities")
 
+id2x2 = [[1., 0.],
+             [0., 1.]]
 
 def test_2x2_identity():
     qscode = ("import Utilities.Matrices.Identity.*;"
@@ -8,5 +14,32 @@ def test_2x2_identity():
 
     result = qsharp.eval(qscode)
 
-    assert result == [[1., 0.],
-                      [0., 1.]]
+    assert result == id2x2
+    
+
+@pytest.mark.parametrize("matrix", [
+    [[1., 0.],
+     [0., 1.]],
+    [[2., 1.],
+     [3., 4.]],
+    [[2., 1.],
+     [3., 4.],
+     [5., 6.]] 
+])
+def test_apply_2x2_identity_happy(matrix: list[list[float]]):
+    qscode = matrix_dot_product_code(matrix, id2x2)
+
+    result = qsharp.eval(qscode)
+
+    assert result == matrix
+
+
+@pytest.mark.parametrize("matrix", [
+    [[1.]]
+])
+def test_apply_2x2_identity_sad(matrix: list[list[float]]):
+
+    qscode = matrix_dot_product_code(matrix, id2x2)
+
+    with pytest.raises(qsharp.QSharpError):
+        qsharp.eval(qscode)
