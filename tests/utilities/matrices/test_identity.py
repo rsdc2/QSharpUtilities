8921dcc66b000f637f731ef662a929657bb2def7
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2025-01-13 20:34:20 +0000

    Add test to check identity matrix works with arguments switched

--- tests/utilities/matrices/test_identity.py
+++ tests/utilities/matrices/test_identity.py
@@ -45,6 +45,25 @@ def test_apply_2x2_identity_happy(matrix: list[list[float]]):
 
 
 @pytest.mark.parametrize("matrix", [
+    [[1., 0.],
+     [0., 1.]],
+    [[2., 1.],
+     [3., 4.]],
+     [[2., 3., 5.],
+      [1., 4., 6.]]
+])
+def test_apply_2x2_identity_switched_happy(matrix: list[list[float]]):
+    """
+    Same as previous test, but with the operands switched
+    """
+    qscode = matrix_dot_product_code(id2x2, matrix)
+
+    result = qsharp.eval(qscode)
+
+    assert result == matrix
+
+
+@pytest.mark.parametrize("matrix", [
     [[1.]]
 ])
 def test_apply_2x2_identity_sad(matrix: list[list[float]]):
